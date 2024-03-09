0543d83a9f0021664fa4ac3bbc830951681fcee4
LuisDiazR-Dev <diazr.info@gmail.com>
2024-03-09 00:19:31 -0500

    structured the hero section and design cleaning

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,57 @@
+import styled from "styled-components"
 
+
+
+
+const about =
+{
+    imgProfile: 'https://ludirez.github.io/img/FotoDePerfil.png',
+    name: 'Luis Díaz-R',
+    detailsA: 'Como ',
+    emphasis: 'Desarrollador Web Full Stack',
+    //todo meter Self en detalles B
+    detailsB: ', encuentro verdadera satisfacción en la creación continua de soluciones basadas en software sólidas, efectivas, livianas y flexibles.',
+    linkedIn: 'https://www.linkedin.com/in/luis-diazr/',
+    gitHub: 'https://github.com/LuisDiazR-Dev'
+}
 
 
 const Hero = () => {
     return (
-        <section>
-            <h1>Hola, Soy Luis Díaz-R</h1>
-            <p>Desarrollador Web Full Stack</p>
-        </section>
+        <SectionStyled class="">
+            <img src={about.imgProfile} alt="foto de perfil" />
+            <span className="badge rounded-pill text-bg-danger ms-2">Disponible para trabajar</span>
+
+            <h1>Hola, Soy {about.name} </h1>
+            <p>{about.detailsA} <strong>{about.emphasis}</strong>{about.detailsB}</p>
+
+            <a href={about.linkedIn} target="_blank" rel="noopener noreferrer" >LinkedIn</a>
+            <a href={about.gitHub} target="_blank" rel="noopener noreferrer" >GitHub</a>
+        </SectionStyled>
+
     )
 }
 
 export default Hero
+
+
+
+const SectionStyled = styled.section`
+/* border: 1px solid #80808055; */
+max-width: 60ch;
+/* text-wrap: balance; */
+/* text-wrap: pretty; */
+
+
+img{
+    width: 60px;
+}
+
+strong{
+    /* color: #7E1328; */
+    color: crimson;
+}
+a{
+    padding-right: 12px;
+}
+`
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,10 @@
 import styled from "styled-components"
 
 const NavbarStyled = styled.nav`
-background-color: #dfdede;
+/* background-color: #dfdede; */
+border: 1px solid #80808055;
+
 border-radius: 25px;
-/* display: flex; */
-/* width: 356px; */
-/* margin: auto; */
-/* padding: 0 10px; */
-/* text-align: center; */
-/* justify-content: space-evenly; */
-/* align-items: center; */
 font-size: 0.9rem;
 
 button{
@@ -33,10 +28,10 @@ const Navbar = () => {
   return (
     <div className="text-center ">
       <NavbarStyled className="row  mx-auto mt-3 mb-3  p-2   d-inline-flex  row-cols-auto">
-        <a className='text-decoration-none ' href="/experiencia">Experiencia</a>
-        <a className='text-decoration-none ' href="/proyectos">Proyectos</a>
-        <a className='text-decoration-none ' href="/sobreMi">Sobre mí</a>
-        <a className='text-decoration-none ' href="/contacto">Contacto</a>
+        <a className='text-decoration-none ' href="https://www.linkedin.com/in/luis-diazr/">Experiencia</a>
+        <a className='text-decoration-none ' href="https://github.com/LuisDiazR-Dev">Proyectos</a>
+        <a className='text-decoration-none ' href="https://www.linkedin.com/in/luis-diazr/">Sobre mí</a>
+        <a className='text-decoration-none ' href="https://www.linkedin.com/in/luis-diazr/">Contacto</a>
       </NavbarStyled>
 
     </div>
