716783765ad6da7ff58d84c123f8a2bae0c48835
LuisDiazR-Dev <diazr.info@gmail.com>
2024-04-12 10:57:09 -0500

    ajusted en redaccion enla seccion sobre mi. ajuste a la direccion del logo ya que no se estaba mostrando

--- src/Components/SobreMi.jsx
+++ src/Components/SobreMi.jsx
@@ -12,28 +12,32 @@ const SobreMi = () => {
 			<div className="row mt-3">
 				<div className="col-xs-12 col-sm-8">
 					<p>
-						Crecí con mi hermano mayor, quien me introdujo al fascinante mundo de las computadoras y al juego de estrategia Age of Empires.
-						<span> A partir de esa experiencia, he desarrollado un talento para buscar patrones óptimos en todas mis actividades,</span> y encontré en el desarrollo web el espacio
+						Crecí con mi hermano mayor, quien me introdujo al fascinante mundo de las computadoras y al juego de estrategia <em> Age of Empires</em>.
+						<span> A partir de esa experiencia, he desarrollado un talento para buscar patrones óptimos en todas mis actividades</span>, y encontré en el desarrollo web el espacio
 						perfecto para aprovechar esta aptitud.
 					</p>
 					<p>
-						A lo largo de mi adolescencia, exploré diferentes áreas académicas, incluyendo <span> mecánica y electrónica,</span> pero no encontré mi verdadera pasión hasta que me
-						reencontré con el desarrollo web hace un año. <span>Desde entonces, he dedicado mi tiempo a perfeccionar mis habilidades </span> en esta área.
+						A lo largo de mi adolescencia, exploré áreas académicas como <span> la mecánica y la electrónica,</span> y aunque aprendí y me parecieron dignos oponentes, no me halle en
+						ellas.
 					</p>
 					<p>
-						Soy un padre dedicado de dos hijos y disfruto pasar tiempo en familia. En mi tiempo libre,
-						<span>me gusta salir en rutas largas con mi bicicleta, lo que me permite relajarme y mantenerme en forma.</span>
+						Hace 2 años me reencontré con el desarrollo web y <span>desde entonces, he dedicado mi tiempo a perfeccionar mis habilidades </span> en esta área en la que, en definitiva, me
+						siento a gusto y a pesar de haber empezado con el desarrollo en WordPress, encuentro fascinante el <span>desarrollo de soluciones basadas en software</span>.
+					</p>
+					<p>
+						Soy un padre dedicado de dos hijos y disfruto pasar tiempo en familia. En mi tiempo libre
+						<span> me gusta salir en rutas largas con mi bicicleta, lo que me permite relajarme y mantenerme en forma</span>.
 					</p>
 					<p>
 						<span>Mi objetivo profesional</span> es contribuir a proyectos de software innovadores y resolver problemas complejos utilizando tecnología, con un especial interés en
 						proyectos relacionados con inteligencia artificial.
 					</p>
 					<p>
-						<span>Estoy emocionado por la oportunidad de colaborar en un entorno desafiante y dinámico.</span>
+						<span>Siempre emocionado por la oportunidad de colaborar en un entorno desafiante y dinámico.</span>
 					</p>
 				</div>
 				<div className=" imgBox col-xs-12 col-sm-4 px-0">
-					<ImgStyled className="rounded" src="/src/assets/logoSelf-RuleTheWorld.jpg" alt="Logo Self" />
+					<ImgStyled className="rounded" src="https://raw.githubusercontent.com/LuisDiazR-Dev/LuisDiazR-Dev.github.io/main/src/assets/logoSelf-RuleTheWorld.jpg" alt="Logo Self" />
 				</div>
 			</div>
 		</SectionStyled>
