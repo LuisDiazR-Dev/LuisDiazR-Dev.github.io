e74cbf3304da54e7814d31f84c949f7e8fbae730
LuisDiazR-Dev <diazr.info@gmail.com>
2024-12-04 01:13:50 -0500

    creando estructura y estilos básicos

---
+++ src/Sections/aNavMenu/NavMenu.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+export const NavMenu = () => {
+	return (
+		<NavbarStyled>
+			<h2>Seré la Navbar</h2>
+		</NavbarStyled>
+	)
+}
+
+
+
+
+
+const NavbarStyled = styled.section`
+  background-color: #282c34;
+
+`
---
+++ src/Sections/zFooter/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Footer = () => {
+	return (
+		<FooterStyled>
+			<h2>Soy le footer</h2>
+		</FooterStyled>
+	)
+}
+
+export default Footer
+
+
+
+const FooterStyled = styled.section`
+  background-color: #282c34;
+
+`
