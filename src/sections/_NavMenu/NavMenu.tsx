e9f33890a9bb6ba68040932499791fcb3e2914f7
LuisDiazR-Dev <diazr.info@gmail.com>
2025-08-23 04:47:33 -0500

    feat: enhance styling and layout; add Brand component to NavMenu; update AboutMeCV description for clarity

--- src/sections/_NavMenu/NavMenu.tsx
+++ src/sections/_NavMenu/NavMenu.tsx
@@ -1,5 +1,5 @@
 import { Button, Navbar } from 'flowbite-react'
-import { HomeIcon } from '../../assets/icons'
+import { Brand } from '../../assets/components/Brand'
 
 // import styled from 'styled-components'
 
@@ -7,38 +7,37 @@ export const NavMenu = () => {
 	return (
 		<Navbar
 			fluid
-			className="bg-gray-100 pt-4
-			rounded-xl  rounded-t-none 			
-			border border-t-0
+			className="
+			pt-4
+			md:max-w-fit mx-auto
+			max-w-full
 			
+
+			bg-white-100 backdrop-blur-sm 
+			fixed top-0 left-0 right-0 z-40
+			rounded-full
+			shadow-sm
+						
 			"
 		>
-			{/* #F5F5F5 */}
-			<Navbar.Brand href="">
-				<img
-					src="/src/assets/self_FavIcon.webp"
-					className=" h-6 sm:h-9"
-					alt="Logo"
-				/>
-				<span className="self-center whitespace-nowrap  font-normal dark:text-white">
-					<i>Dev.Solutions </i>
-				</span>
+			<Navbar.Brand href="#sobre-mi">
+				<div className="mr-3 border-r pr-3 border-gray-300">
+					<Brand />
+				</div>
 			</Navbar.Brand>
-			<div className="flex md:order-2">
+			<div className="flex md:order-2 md:mx-5  space-x-2">
 				<Button>¡Hablemos!</Button> {/* modal */}
 				<Navbar.Toggle />
 			</div>
 			<Navbar.Collapse>
-				<Navbar.Link href="/" active>
-					<HomeIcon className="w-4 h-4 text-gray-800 dark:text-gray-400" />
+				<Navbar.Link href="#" active>
+					Experiencia
 				</Navbar.Link>
-				<Navbar.Link href="#">Sobre mi</Navbar.Link>
-				<Navbar.Link href="#">Experiencia</Navbar.Link>
 				<Navbar.Link href="#">proyectos</Navbar.Link>
 				<Navbar.Link href="#">Stack</Navbar.Link>
 				<Navbar.Link href="#">Servicios</Navbar.Link>
 				<Navbar.Link href="#" disabled>
-					Blog
+					Tema
 				</Navbar.Link>
 			</Navbar.Collapse>
 		</Navbar>
