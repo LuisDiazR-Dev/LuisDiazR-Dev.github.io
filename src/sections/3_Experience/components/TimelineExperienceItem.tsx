014bec929d6ef75a03c945ed7b8098a0bb152b3a
LuisDiazR-Dev <diazr.info@gmail.com>
2025-08-26 02:08:39 -0500

    refactor: remove unused summary and details variables; clean up commented-out code in TimelineExperienceItem component

--- src/sections/3_Experience/components/TimelineExperienceItem.tsx
+++ src/sections/3_Experience/components/TimelineExperienceItem.tsx
@@ -20,7 +20,6 @@ import { useState } from 'react'
 
 export const TimelineExperienceItem = () => {
 	const [openIndex, setOpenIndex] = useState<number | null>(null)
-	const [summary, ...details] = experienceList.flatMap((exp) => exp.description)
 
 	return (
 		<>
@@ -106,50 +105,3 @@ export const TimelineExperienceItem = () => {
 		</>
 	)
 }
-
-// const ExperienceItem = styled.li`
-// 	/* border: 1px solid blue;
-// 	max-width: 360px; */
-// 	header {
-// 		display: flex;
-// 		gap: 12px;
-// 		align-items: center;
-// 	}
-// `
-// const anterior = () => {
-// 	return (
-// 		<>
-// 			{experienceList.map((exp, index) => (
-// 				<ExperienceItem key={index} className="mb-10 ms-4">
-// 					<div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-// 					<header className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-// 						<time>
-// 							{exp.startDate}
-// 							{exp.endDate && <span>{` - ${exp.endDate}`}</span>}
-// 						</time>
-// 						<h3 className="font-mono">
-// 							<a
-// 								href={exp.companyURL}
-// 								target="_blank"
-// 								rel="noopener noreferrer"
-// 							>
-// 								| {exp.company}
-// 							</a>
-// 						</h3>
-// 					</header>
-// 					<section>
-// 						<h4 className="text-lg font-semibold text-gray-900 dark:text-white">
-// 							{exp.title}
-// 						</h4>
-// 						<p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-// 							{exp.description[0]}
-// 						</p>
-// 					</section>
-// 					<footer>
-// 						<a href="#">Leer más</a>
-// 					</footer>
-// 				</ExperienceItem>
-// 			))}
-// 		</>
-// 	)
-// }
